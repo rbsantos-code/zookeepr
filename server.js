1bbc41488c5274c399f9da9f7d4f2f4239086a35
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-04-05 11:05:25 -0700

    start filter function

--- server.js
+++ server.js
@@ -7,13 +7,30 @@ const { animals } = require('./data/animals.json')
 
 
 
-
+// handle filter functionality 
+function filterByQuery(query, animalsArray) {
+    let filteredResults = animalsArray;
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
 
 
 
 // add the route
 app.get('/api/animals', (req, res) => {
-    res.json(animals)
+    let results = animals;
+    if (req.query) { // req.query as an argument and filter through the animals accordingly - 11.1.5
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results)
 });
 
 // add port
