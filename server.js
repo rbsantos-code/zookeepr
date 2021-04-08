3e868e79d91b7cf407428daaec59fd2f27730832
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-04-08 13:07:19 -0700

    create route animals and zookeepr

--- server.js
+++ server.js
@@ -131,6 +131,16 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/zookeepr-public/index.html'));
 });
 
+// add route for animals html - 11.3.6
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepr-public/animals.html'));
+});
+
+// add route for zookeepers html - 11.3.6
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepr-public/zookeepers.html'));
+});
+
 // add port
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
